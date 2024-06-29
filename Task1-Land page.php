2563c60fce1e390af390585fa1340df208521994
POOJA KUSHWAHA <poojakushwaha52797@gmail.com>
2024-06-29 16:54:26 +0530

    Add files via upload

---
+++ Task1-Land page.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website</title>
+    <link rel="stylesheet" href="land.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Add Font Awesome and other dependencies here -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link rel="icon" href="Favicon.png">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+
+    <style>
+    </style>
+
+    <?php include ('includes/nav.php'); ?>
+
+</head>
+
+<body>
+    <br>
+    <!-- Your existing HTML content for the homepage -->
+    <img src="images/firpic.png " width="100%">
+    <div class="fli">
+        <p class="heading">The Benefits of Booking an Artist Through YourLiveArtist</><br><br>
+        <p class="description">When you book an artist through our platform, you'll enjoy:</p>
+
+        <div class="flipcard" style="margin-left: 210px;">
+            <div class="maincontainer">
+                <div class="back">
+                    <h4>Affordable Pricing</h4>
+
+                    <p>We work hard to make sure our artists are compensated fairly while also keeping our
+                        prices
+                        competitive.<br><br></p>
+                </div>
+                <div class="front">
+                    <div class="image">
+                        <img src="images/Asset 8.png">
+                        <h2>Comedy Entertain</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="maincontainer">
+                <div class="back">
+                    <h4> Quality Performances</h4>
+
+                    <p>All of our artists are carefully vetted to ensure that they meet our high standards for
+                        talent
+                        and professionalism.<br></p>
+                </div>
+                <div class="front">
+                    <div class="image">
+                        <img src="images/Asset 10.png">
+                        <h2>Singing</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="maincontainer">
+                <div class="back">
+                    <h4> A Memorable Experience </h4>
+                    <p>With YourLiveArtist, you'll create unforgettable memories with a high-quality live
+                        performance
+                        that you and your guests will never forget.</p>
+                </div>
+                <div class="front">
+                    <div class="image">
+                        <img src="images/Asset 9.png">
+                        <h2>Tale Writing</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+
+    <!-- Include Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <div class="reviews">
+        <p class="heading">What Our Happy Users and Artists Have to Say</><br><br>
+        <p class="description">Don't just take our word for it - hear from some of our satisfied users and artists:</p>
+
+        <!-- Carousel -->
+        <div id="testimonialCarousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carousel-caption">
+                        <p>"I found an amazing artist for my daughter's birthday party through YourLiveArtist. The
+                            musician was affordable, professional, and so talented!"<br><br><br><br><br><br>
+
+                            - Sarah K.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption">
+                        <p>"As an artist, YourLiveArtist has been a game changer for me. I've gotten so many more gigs
+                            through the platform and have made more money than I ever thought possible. Thank
+                            you!"<br><br><br><br><br>
+                            - Tim R.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption">
+                        <p>"I've used YourLiveArtist for several events now and I couldn't be happier with the results.
+                            The quality of the performers is top-notch and the booking process is a breeze. Highly
+                            recommend!"<br><br><br><br>
+                            - Jason B.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="carousel-control-prev" href="#testimonialCarousel" role="button" data-slide="prev">
+                <span class="carousel-cosntrol-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous <- </span>
+            </a>
+            <a class="carousel-control-next" href="#testimonialCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <!-- Include Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script type="text/javascript" src="car-script.js"></script>
+    <br><br>&nbsp;
+
+
+
+    <div class="Contactpic">
+        <img src="images/end.jpg" width="100%">
+    </div>
+
+    <div class="cont-text">
+    <div class="contact-container">
+        <div class="contact-map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1578265.0941403757!2d-98.9828708842255!3d39.41170802696131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sin!4v1407515822047">
+            </iframe>
+        </div>
+        <div class="vertical-card">
+            <div class="card-content">
+                <h2>Contact Us</h2>
+                <p>Ready to book your next live performance? Have questions about our platform? Contact us
+                    today -
+                    we're always happy to help.</p>
+                <a href="#contact" class="arrow-button"><i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+    <div class="sec">
+        <div class="contact-cards container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card2">
+                        <span class="icon">
+                            <i class="fa fa-phone"></i>
+                        </span>
+                        <div class="content-wrap">
+                            <span class="item-title">
+                                Call us anytime
+                            </span>
+                            <p class="text">
+                                +386 5555 235 / +386 8754 231
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card2">
+                        <span class="icon">
+                            <i class="fa fa-map"></i>
+                        </span>
+                        <div class="content-wrap">
+                            <span class="item-title">
+                                Our headquarters </span>
+                            <p class="text">
+                                300 Pennsylvania Ave NW,
+                                Washington, DC 20006, USA
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card2">
+                        <span class="icon">
+                            <i class="fa fa-envelope"></i>
+                        </span>
+                        <div class="content-wrap">
+                            <span class="item-title">
+                                E-Mail us
+                            </span>
+                            <p class="text">
+                                +jhonedoe@gmail.com /
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <?php include ('includes/footer.php'); ?>
+
+</body>
+
+</html>
